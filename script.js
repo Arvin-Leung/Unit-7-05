61f87a65662d4156fa3f69f4ceaedde01f5b5f1d
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-22 18:53:54 +0000

    space added

--- script.js
+++ script.js
@@ -2,13 +2,11 @@
 document.getElementById('click').addEventListener('click', checker)
 
 // This function tells the user if there is school/work or not
-function checker() {
+function checker () {
   // This lines below store the inputs into the variables
   const date = document.getElementById('date').value
   const age = document.getElementById('age').value
   // The condition below checks if they are a student or not
-  console.log(date)
-  console.log(age)
   if (((date === 'saturday') || (date === 'sunday')) && (age <= 17)) {
     document.getElementById('result').innerHTML = 'It is not a weekday, No school!'
   } else if (((date != 'saturday') || (date != 'sunday')) && (age <= 17)) {
@@ -19,6 +17,3 @@ function checker() {
     document.getElementById('result').innerHTML = 'It is weekday, Time for Work!'
   }
 }
-
-  
-
